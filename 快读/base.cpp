f5c8defd2cc6d3cac52dc7d4208e060e52f0eb79
CairBin <1147627792@qq.com>
2023-10-17 01:45:49 +0800

    Add quick read

---
+++ 快读/base.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define re register
+#define il inline
+
+il int read()
+{
+    re int x = 0, f = 1;
+    char c = getchar();
+    while (c < '0' || c > '9')
+    {
+        if (c == '-')
+            f = -1;
+        c = getchar();
+    }
+
+    while (c >= '0' && c <= '9')
+    {
+        x = (x << 3) + (x << 1) + (c ^ 48);
+        c = getchar();
+    }
+
+    return x * f;
+}
